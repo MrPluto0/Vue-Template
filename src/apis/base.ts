5d4f75958ad5260698dbfff2aff2e1dc3d575d9a
Gypsophlia <710852553@qq.com>
2022-12-04 20:39:54 +0800

    feat: update http format

--- src/apis/base.ts
+++ src/apis/base.ts
@@ -1,14 +1,25 @@
 import axios from "axios";
+import { transformCamel, transformSnake } from "@/utils/transform";
 
 export const service = axios.create({
   baseURL: "/api",
 });
 
+service.interceptors.request.use(
+  function (config) {
+    config.data = transformSnake(config.data);
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
 service.interceptors.response.use(
   function (response) {
     const resp = response.data;
     if (resp.code === 200) {
-      return resp.data || resp.message;
+      return transformCamel(resp.data || resp.message);
     } else {
       return Promise.reject(resp);
     }
