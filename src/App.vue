22d22ba9446db1c2513b0de546feb67d9546fbd9
Gypsophlia <710852553@qq.com>
2022-12-04 16:43:41 +0800

    feat: update router-view

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <div class="header">Header</div>
-    <HelloWorld />
+    <router-view v-slot="{ Component }">
+      <keep-alive>
+        <component :is="Component" :key="$route.name" />
+      </keep-alive>
+    </router-view>
   </div>
 </template>
 
-<script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="less">
 .header {
